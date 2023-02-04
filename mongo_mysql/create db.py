3ea4ba6fc7f1365237052fc38ec00fa4ab84d1b5
darshita2304 <dppaghadal@gmail.com>
2023-02-04 12:10:27 +0530

    Initial commit

---
+++ mongo_mysql/create db.py
@@ -0,0 +1,13 @@
+import mysql.connector
+# import mysql.connector
+#create user 'user'@'%' identified by 'password'
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="abc",
+  password="password"
+)
+
+cur = mydb.cursor()
+
+cur.execute("create database new_db")
+
