89b622c4babc5c59a09741849f41d78900a13b7f
darshita2304 <dppaghadal@gmail.com>
2023-07-14 21:54:23 +0530

    dl

--- flask_scrapper/app.py
+++ flask_scrapper/app.py
@@ -1,61 +1,69 @@
-from flask import Flask, render_template, request,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, jsonify
+from flask_cors import CORS, cross_origin
 import requests
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen as uReq
 import logging
-logging.basicConfig(filename="scrapper.log" , level=logging.INFO)
+import csv
+logging.basicConfig(filename="scrapper.log", level=logging.INFO)
 
 app = Flask(__name__)
 
-@app.route("/", methods = ['GET'])
+
+@app.route("/", methods=['GET'])
 def homepage():
     return render_template("index.html")
 
-@app.route("/review" , methods = ['POST' , 'GET'])
+
+@app.route("/review", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         try:
-            searchString = request.form['content'].replace(" ","")
+            searchString = request.form['content'].replace(" ", "")
             flipkart_url = "https://www.flipkart.com/search?q=" + searchString
             uClient = uReq(flipkart_url)
             flipkartPage = uClient.read()
             uClient.close()
             flipkart_html = bs(flipkartPage, "html.parser")
-            bigboxes = flipkart_html.findAll("div", {"class": "_1AtVbE col-12-12"})
+            bigboxes = flipkart_html.findAll(
+                "div", {"class": "_1AtVbE col-12-12"})
             del bigboxes[0:3]
             box = bigboxes[0]
-            productLink = "https://www.flipkart.com" + box.div.div.div.a['href']
+            productLink = "https://www.flipkart.com" + \
+                box.div.div.div.a['href']
             prodRes = requests.get(productLink)
-            prodRes.encoding='utf-8'
+            prodRes.encoding = 'utf-8'
             prod_html = bs(prodRes.text, "html.parser")
             print(prod_html)
             commentboxes = prod_html.find_all('div', {'class': "_16PBlm"})
 
             filename = searchString + ".csv"
-            fw = open(filename, "w")
-            headers = "Product, Customer Name, Rating, Heading, Comment \n"
-            fw.write(headers)
+            # fw = open(filename, "w")
+            # headers = "Product, Customer Name, Rating, Heading, Comment \n"
+            csv_columns = ["Product", "Customer Name",
+                           "Rating", "Heading", "Comment"]
+
+            # fw.write(headers)
             reviews = []
             for commentbox in commentboxes:
                 try:
-                    #name.encode(encoding='utf-8')
-                    name = commentbox.div.div.find_all('p', {'class': '_2sc7ZR _2V5EHH'})[0].text
+                    # name.encode(encoding='utf-8')
+                    name = commentbox.div.div.find_all(
+                        'p', {'class': '_2-N8zT'})[0].text
 
                 except:
                     logging.info("name")
 
                 try:
-                    #rating.encode(encoding='utf-8')
+                    # rating.encode(encoding='utf-8')
                     rating = commentbox.div.div.div.div.text
-
 
                 except:
                     rating = 'No Rating'
                     logging.info("rating")
 
                 try:
-                    #commentHead.encode(encoding='utf-8')
+                    # commentHead.encode(encoding='utf-8')
                     commentHead = commentbox.div.div.div.p.text
 
                 except:
@@ -63,16 +71,33 @@ def index():
                     logging.info(commentHead)
                 try:
                     comtag = commentbox.div.div.find_all('div', {'class': ''})
-                    #custComment.encode(encoding='utf-8')
+                    # custComment.encode(encoding='utf-8')
                     custComment = comtag[0].div.text
                 except Exception as e:
                     logging.info(e)
 
                 mydict = {"Product": searchString, "Name": name, "Rating": rating, "CommentHead": commentHead,
                           "Comment": custComment}
+
+                # fw.write(mydict)
+                # for key in mydict.keys():
+                #     fw.write("%s,%s\n"%(key,mydict[key]))
+
                 reviews.append(mydict)
+
+            try:
+                with open(filename, 'w') as csvfile:
+                    writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+                    writer.writeheader()
+                    for data in reviews:
+                        writer.writerow(data)
+                        logging.info("writing data... {}".format(data))
+            except IOError:
+                print("I/O error")
+                logging.info("log my final result {}".format(reviews))
+
             logging.info("log my final result {}".format(reviews))
-            
+
             return render_template('result.html', reviews=reviews[0:(len(reviews)-1)])
         except Exception as e:
             logging.info(e)
@@ -83,5 +108,5 @@ def index():
         return render_template('index.html')
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
